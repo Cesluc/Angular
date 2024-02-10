e6269e467bbe1485b6a862a213777d306bff15c9
Cesluc <clucumi.94@gmail.com>
2024-02-09 22:22:38 -0500

    Desestructuracion de Arreglos

---
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,38 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author: "Ed Sheeran",
+        year: 2015
+    }
+}
+
+// const { song } = audioPlayer;
+const { song: anotherSong, songDuration: duration, details } = audioPlayer;
+
+const {author} = details;
+
+
+
+// console.log('Song: ', audioPlayer.song);
+console.log('Song: ', anotherSong);
+console.log('Duration: ', duration);
+// console.log('Song: ', audioPlayer.details.author);
+console.log('Author: ', author);
+
+
+
+export {};
