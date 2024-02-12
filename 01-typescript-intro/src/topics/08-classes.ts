329f2066b6447869e295caec0ecec2149e2b4ea8
Cesluc <clucumi.94@gmail.com>
2024-02-12 14:48:33 -0500

    Priorizar composicion sobre herencia

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -5,22 +5,38 @@ export class Person {
     // private address: string;
 
     constructor(
-        public name: string,
+        public firstName: string,
+        public lastName: string,
         private address: string = 'No Address'
     ) { }
 
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) {
+//         super(realName, 'NewYork')
+//     }
+// }
+
+export class Hero {
+    
+    // public person: Person;
+
     constructor(
         public alterEgo: string,
         public age: number,
         public realName: string,
+        public person: Person,
     ) {
-        super(realName,'NewYork')
+        // this. person = new Person( realName)
     }
 }
 
-
-const ironman = new Hero('Ironman',45,'Tony Stark');
+const tony = new Person('tony','stark', 'Newyork')
+const ironman = new Hero('Ironman',45,'Tony Stark', tony);
 console.log(ironman);
